b319205af206f5c1069b83d9793187e067569e61
MeroMemo <1043526080@qq.com>
2024-09-04 01:57:38 +0800

    Object对象

--- ts-demo/003_Object_object_{}/index.ts
+++ ts-demo/003_Object_object_{}/index.ts
@@ -1,5 +1,8 @@
-namespace module_003_Object_object_{} {
-
-    
+namespace Module_003_Object_object_ { } {
+    let a: Object = 123;
+    let b: Object = '123';
+    let c: Object = [];
+    let d: Object = {};
+    let e: Object = () => 3;
 
 }
