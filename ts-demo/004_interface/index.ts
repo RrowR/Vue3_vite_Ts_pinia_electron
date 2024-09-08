14830604b2f1403a12b28d65d7f8ad8c57cfa602
Memo <2415782224@qq.com>
2024-09-09 07:05:45 +0800

    更新ts接口

--- ts-demo/004_interface/index.ts
+++ ts-demo/004_interface/index.ts
@@ -5,10 +5,15 @@ namespace module_001_接口类型 {
 
     interface Person {      // 相同接口可以合并属性
         Ikun: string;
+        [name: string]: any;          // 索引签名： 可选属性，properName为属性名，any为属性值类型
     }
 
     let a: Person = {       // 定义的对象必须拥有对应的属性，不能少
         name: '张三',
-        Ikun: '蔡徐坤'
+        Ikun: '蔡徐坤',
+        'age': 18,      // 
+        'age2': 18,
+        'age1': 18
     }
+    console.log(a);
 }
